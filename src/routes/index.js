5c1532d5622e110b1e49927c211c0a986ad378d8
Quan <quan.di1207@gmail.com>
2024-02-26 15:57:06 +0700

    Refactor code: create routes, app/controllers folders

---
+++ src/routes/index.js
@@ -0,0 +1,30 @@
+const newsRouter = require('./news')
+const siteRouter = require('./site')
+
+
+function route(app) {
+  app.use('/news', newsRouter)
+
+  app.use('/', siteRouter)
+
+  // app.use('/search', siteRouter)
+
+  // app.get('/', (req, res) => {
+  //   res.render('home')
+  // })
+  
+  // app.get('/news', (req, res) => {
+  //   res.render('news')
+  // })
+  
+  // app.get('/search', (req, res) => {
+  //   res.render('search')
+  // })
+  
+  // app.post('/search', (req, res) => {
+  //   console.log(req.body)
+  //   res.send('')
+  // })
+}
+
+module.exports = route
