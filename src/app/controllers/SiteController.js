8f031c98c07a21c928daf58c53cd4ca209283d7b
Quan <quan.di1207@gmail.com>
2024-02-28 10:59:23 +0700

    CRUD Read courses from Db

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,9 +1,25 @@
+const Course = require('../models/Course')
+const { multipleMongooseToObject } = require('../../util/mongoose')
 
 class SiteController {
-
   // [GET] /
-  index(req, res) {
-    res.render('home')
+  // async index(req, res) {
+  //   try {
+  //     const courses = await Course.find({})
+  //     res.json(courses)
+  //   } catch (error) {
+  //     res.status(400).json({ error: 'ERROR' })
+  //   }
+  // }
+
+  index(req, res, next) {
+    Course.find({})
+      .then((courses) => {
+        res.render('home', {
+          courses: multipleMongooseToObject(courses),
+        })
+      })
+      .catch(next)
   }
 
   // [GET] /search
@@ -12,4 +28,4 @@ class SiteController {
   }
 }
 
-module.exports = new SiteController
+module.exports = new SiteController()
--- src/index.js
+++ src/index.js
@@ -7,27 +7,36 @@ const app = express()
 const port = 3000
 
 const route = require('./routes')
+const db = require('./config/db/index')
+
+//Connect DB
+db.connect()
 
 app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.urlencoded({
-  extended: true
-}))
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+)
 app.use(express.json())
 
 //HTTP logger
 app.use(morgan('dev'))
 
 // Template engine
-app.engine('hbs', engine({
-  extname: '.hbs'
-}))
+app.engine(
+  'hbs',
+  engine({
+    extname: '.hbs',
+  })
+)
 
 app.set('view engine', 'hbs')
-app.set('views', path.join(__dirname, 'resources/views'))
+app.set('views', path.join(__dirname, 'resources', 'views'))
 
 //Routes init
-route(app);
+route(app)
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App listening on port ${port}`)
 })
